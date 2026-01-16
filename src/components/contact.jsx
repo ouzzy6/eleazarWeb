571469a45c7ed07b768ceb9d3370da44ca0412ae
Ouzzy6 <ouzzy6@gmail.com>
2026-01-16 17:56:26 +0100

    emailjs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,7 +20,7 @@ function Contact() {
         e.preventDefault();
 
         // Reemplaza estos valores con los tuyos
-        const serviceID = "service_euhz6lq";
+        const serviceID = "service_oi1hmi9";
         const templateID = "template_e8a7pjh";
         const publicKey = "pu5nU8PjLwSpoWbEK";
 
